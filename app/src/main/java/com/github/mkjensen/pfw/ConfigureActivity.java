c70e2b841e2e4c913817af5b389bab17581af371
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2016-10-22 12:27:25 +0200

    Initial commit

---
+++ app/src/main/java/com/github/mkjensen/pfw/ConfigureActivity.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2016 Martin Kamp Jensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mkjensen.pfw;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.File;
+
+public class ConfigureActivity extends AppCompatActivity {
+
+  private static final int OPEN_IMAGE_REQUEST_CODE = 42;
+
+  private int widgetId;
+
+  private File imageFile;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // User may cancel (e.g. press back button) at any time. Default to canceled result.
+    setResult(RESULT_CANCELED);
+
+    // Initialization.
+    initWidgetId();
+    initImageFile();
+
+    // Request image from user.
+    openImage();
+  }
+
+  private void initWidgetId() {
+    int widgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+      widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+    if (widgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+      // Widget ID missing, abort.
+      // TODO: Report problem
+      finish();
+    }
+    this.widgetId = widgetId;
+  }
+
+  private void initImageFile() {
+    imageFile = Pfw.getImageFile(this, widgetId);
+  }
+
+  private void openImage() {
+    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+    intent.addCategory(Intent.CATEGORY_OPENABLE);
+    intent.setType(Pfw.ALL_IMAGES_MIME_TYPE);
+    startActivityForResult(intent, OPEN_IMAGE_REQUEST_CODE);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == OPEN_IMAGE_REQUEST_CODE) {
+      imageOpened(resultCode, data);
+    } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+      imageCropped(resultCode);
+    }
+  }
+
+  private void imageOpened(int resultCode, Intent data) {
+    if (resultCode != RESULT_OK) {
+      // TODO: Report problem
+      new AlertDialog.Builder(this).setMessage("An error occurred while opening the image").show();
+      finish();
+      return;
+    }
+    cropImage(data.getData());
+  }
+
+  private void cropImage(Uri imageUri) {
+    // TODO: Set options such that cropping results in optimal image for widget
+    CropImage.activity(imageUri)
+        .setOutputUri(Uri.fromFile(imageFile))
+        .start(this);
+  }
+
+  private void imageCropped(int resultCode) {
+    if (resultCode != RESULT_OK) {
+      // TODO: Report problem
+      new AlertDialog.Builder(this).setMessage("An error occurred while cropping the image").show();
+      finish();
+      return;
+    }
+    saveAndUpdateWidget();
+  }
+
+  private void saveAndUpdateWidget() {
+    saveWidget();
+    PfwAppWidgetProvider.setImage(this, AppWidgetManager.getInstance(this), widgetId,
+        imageFile.getAbsolutePath());
+
+    finishSuccessful();
+  }
+
+  private void saveWidget() {
+    SharedPreferences.Editor editor = Pfw.getPreferences(this).edit();
+    editor.putString(Pfw.getImagePathPreferenceKey(widgetId), imageFile.getAbsolutePath());
+    editor.apply();
+  }
+
+  private void finishSuccessful() {
+    Intent intent = new Intent();
+    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+    setResult(RESULT_OK, intent);
+    finish();
+  }
+}
---
+++ app/src/main/java/com/github/mkjensen/pfw/PfwAppWidgetProvider.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 Martin Kamp Jensen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mkjensen.pfw;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+public class PfwAppWidgetProvider extends AppWidgetProvider {
+
+  @Override
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    SharedPreferences preferences = Pfw.getPreferences(context);
+    for (int i : appWidgetIds) {
+      String imagePath = preferences.getString(Pfw.getImagePathPreferenceKey(i), null);
+      if (imagePath != null) {
+        setImage(context, appWidgetManager, i, imagePath);
+      } else {
+        showError(context, appWidgetManager, i);
+      }
+    }
+  }
+
+  static void setImage(@NonNull Context context, @NonNull AppWidgetManager manager, int widgetId,
+                       @NonNull String imagePath) {
+    Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+    setBitmap(context, manager, widgetId, bitmap);
+  }
+
+  private static void setBitmap(@NonNull Context context, @NonNull AppWidgetManager manager,
+                                int widgetId, @NonNull Bitmap bitmap) {
+    RemoteViews views = getRemoteViews(context);
+    views.setImageViewBitmap(R.id.widget_image, bitmap);
+    manager.updateAppWidget(widgetId, views);
+  }
+
+  @NonNull
+  private static RemoteViews getRemoteViews(@NonNull Context context) {
+    return new RemoteViews(context.getPackageName(), R.layout.widget);
+  }
+
+  private static void showError(@NonNull Context context, @NonNull AppWidgetManager manager,
+                                int widgetId) {
+    RemoteViews views = getRemoteViews(context);
+    views.setImageViewResource(R.id.widget_image, R.mipmap.ic_launcher);
+    manager.updateAppWidget(widgetId, views);
+  }
+}
